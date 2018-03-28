cc58c3db02f36aea8a68d041651f43f94b66c554
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-03-29 00:37:19 +0900

    1

--- How many fingers am i holding up?/ViewController.swift
+++ How many fingers am i holding up?/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputNumbers: UITextField!
+    @IBOutlet weak var resultNumbers: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func guessNumbers(_ sender: UIButton) {
+        //랜덤 숫자 생성
+        let number = String(arc4random() % 6)
+        if(number == inputNumbers.text!) {
+            resultNumbers.text = "맞아요"
+        } else {
+            resultNumbers.text = "틀렸어요 값은 \(number)입니다"
+        }
+    }
+    
 
 }
 
